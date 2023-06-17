fc159e617b3c57c035f8bff362ae917192de76f8
excaliar <langanmatt55@gmail.com>
2023-06-17 18:44:26 -0400

    create functions to update display upon operations occuring or values changing

--- script.js
+++ script.js
@@ -11,7 +11,14 @@ function multiply(a,b) {
 }
 
 function divide(a,b) {
-    a / b;
+    if (b == 0) {
+        return "÷ by 0 error"
+    }
+    return a / b;
+}
+
+function exponent(a,b) {
+    return a ** b;
 }
 
 function operate(a, b, sign) {
@@ -21,33 +28,86 @@ function operate(a, b, sign) {
     if (sign == '-') {
         return subtract(a,b);
     }
-    if (sign == '*') {
+    if (sign == 'x') {
         return multiply(a,b);
     }
-    if (sign == '/') {
+    if (sign == '÷') {
         return divide(a,b);
+    }
+    if (sign == "^") {
+        return exponent(a,b);
     }
 }
 
-function displayOne(nums) {
+//updates display when number buttons pressed
+//sets variable value
+function display(nums) {
     nums.forEach(num => {
         num.addEventListener('click', () => {
             x = x + num.textContent;
-            if (+x > 0) {
+            if (po == "") {
                 dis.textContent = x;
+            } else {
+                dis.textContent = y + " " + po + " " + x;
             };
         });
     });
 }
 
+//updates display when operand button pressed
+//sets operand value
+function operand(oper) {
+    oper.forEach(op => {
+        op.addEventListener('click', () => {
+            po = op.textContent;
+            dis.textContent = x + ' ' + po;
+            y = x;
+            x = ''
+        });
+    });
+}
+
+//resets all values
+function reset(clear) {
+    clear.addEventListener('click', () => {
+        dis.textContent = "0"
+        x = ''
+        y = ''
+        po = ''
+    })
+}
+
+//performs equation when equals button is pressed
+function equ(equals) {
+    equals.addEventListener('click', () => {
+        dis.textContent = operate(+y, +x, po);
+    })
+}
+
+//technically the second variable during operations
 let x = '';
+//technically the first variable during operations
 let y = '';
+//operand
+let po = '';
+
 
 //number buttons
 nums = document.querySelectorAll(".num");
 //calculator display
 dis = document.querySelector(".dis");
+//operand
+op = document.querySelectorAll(".oper");
+//clear
+clear = document.querySelector(".AC");
+//operations
+equals = document.querySelector(".eq");
 
-displayOne(nums);
+display(nums);
+operand(op);
+reset(clear);
+equ(equals);
+
+
 
 
